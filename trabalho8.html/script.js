129ac802cf1538135533bfeffa146ad6848a4fb1
Vault <marielebruno01@gmail.com>
2026-04-29 16:44:38 -0300

    Add files via upload

---
+++ trabalho8.html/script.js
@@ -0,0 +1,157 @@
+function botao1() {
+    const nome = "Bruno";
+    alert("Bruno");
+}
+
+function botao2() {
+    const a = 10;
+    const b = 6;
+    const soma = a + b;
+    alert(soma);
+}
+
+function botao3() {
+    const textin = document.getElementById("textin");
+    textin.textContent = "Outro texto mucho louco";
+}
+
+function botao4() {
+    const cor = document.getElementById("preso");
+    cor.style.background = "red";
+}
+
+function botao5() {
+    const text1 = document.getElementById("text1");
+    text1.textContent = "Você clicou!";
+}
+
+function botao6() {
+    const text2 = document.getElementById("text2");
+    text2.textContent = " ";
+}
+
+function show() {
+    const showtext = document.getElementById("showtext");
+    showed.textContent = showtext.value;
+}
+
+function soma1() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let result1 = Number(num1) + Number(num2);
+    let local = document.getElementById("result1");
+    local.textContent = result1
+}
+
+function bkc() {
+    const bcor = document.getElementById("booty");
+    bcor.style.background = "aqua";
+} 
+
+function rtn() {
+    const bcor = document.getElementById("booty");
+    bcor.style.background = "linear-gradient(to right, royalblue, whitesmoke)";
+}
+
+function mtmn() {
+    const tmn = document.getElementById("tmn");
+    tmn.style.fontSize = "x-large"
+}
+
+function pcor() {
+    const pcor = document.getElementById("mcor");
+    mcor.style.color = "palevioletred"
+}
+
+function double() {
+    const first = document.getElementById("first");
+    const second = document.getElementById("second");
+    const textchange1 = document.getElementById("textchange1");
+    const textchange2 = document.getElementById("textchange2");
+    first.style.background = "red";
+    second.style.background = "blue";
+    textchange1.textContent = "NA-"
+    textchange2.textContent = "DA!!"
+}
+
+function quintuplets() {
+    const ichika = document.getElementById("ichika");
+    const nano = document.getElementById("nano");
+    const miku = document.getElementById("miku");
+    const yotsuba = document.getElementById("yotsuba");
+    const itsuki = document.getElementById("itsuki");
+    ichika.textContent = "Ichika Nakano";
+    nano.textContent = "Nano Nakano";
+    miku.textContent = "Miku Nakano";
+    yotsuba.textContent = "Yotsuba Nakano";
+    itsuki.textContent = "Itsuki Nakano";
+    ichika.style.color = "pink";
+    nano.style.color = "fuchsia";
+    miku.style.color = "dark";
+    yotsuba.style.color = "orangered";
+    itsuki.style.color = "red";
+}
+
+function tri() {
+    const cosas = document.getElementById("cosas");
+    const minicosas = document.getElementById("minicosas");
+    const minicosas2 = document.getElementById("minicosas2");
+    cosas.style.background = "black";
+    minicosas.style.color = "white";
+    minicosas2.style.color = "white";
+    minicosas2.textContent = "WHoA!1!";
+}
+
+function bts2() {
+    const sla = document.getElementById("duo");
+    duo1.textContent = sla.value;
+    duo2.textContent = sla.value;
+}
+
+function sad() {
+    const trans = document.getElementById("trans");
+    trans.textContent = "Sou queer";
+    trans.style.color = "hotpink";
+    trans.style.fontSize = "xx-large";
+    trans.style.background = "linear-gradient(to right, pink, fuchsia)";
+    trans.style.fontWeight = "bolder";
+}
+
+function ddivs() {
+    const duas1 = document.getElementById("duas1");
+    const duas2 = document.getElementById("duas2");
+    duas1.style.color = "whitesmoke";
+    duas2.style.color = "whitesmoke";
+    duas1.style.background = "black";
+    duas2.style.background = "black";
+}
+
+function copy() {
+    const copie =  document.getElementById("textmit");
+    yuta.textContent = copie.value;
+    yuta.style.color = "navajowhite";
+}
+
+function soma2() {
+    let num3 = document.getElementById("num3").value;
+    let num4 = document.getElementById("num4").value;
+    let result2 = Number(num3) + Number(num4);
+    let local1 = document.getElementById("result2");
+    local1.textContent = result2;
+}
+
+function excluir() {
+    let num3 = document.getElementById("num3").value;
+    let num4 = document.getElementById("num4").value;
+    let result2 = Number(num3) + Number(num4);
+    let local1 = document.getElementById("result2");
+    local1.textContent = " ";
+}
+
+function mexe() {
+    const desastre = document.getElementById("booty");
+    alert("ERROR!!!");
+    desastre.style.background = "black";
+    desastre.style.color = "red";
+    desastre.style.fontSize = "xx-large";
+}
